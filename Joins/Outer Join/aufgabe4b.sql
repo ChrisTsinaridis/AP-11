457a531e65f25c9fb33f72f30d7f0b528f89b4b6
Christos Tsinaridis <christos@tsinaridis.com>
2023-05-05 08:54:17 +0200

    outer joins aufgaben

--- Joins/Outer Join/aufgabe4b.sql
+++ Joins/Outer Join/aufgabe4b.sql
@@ -2,11 +2,11 @@ select distinct
     b.name
 FROM
     bundesland as b 
-left join (
-    kunde as k 
-    inner join ort o on o.id=k.oid 
-    inner join landkreis l on l.id=o.lid
-    ) on b.id=l.bid
+    left join (
+        kunde as k 
+        inner join ort o on o.id=k.oid 
+        inner join landkreis l on l.id=o.lid
+        ) on b.id=l.bid
 where
     k.id is NULL
 order by 1;
